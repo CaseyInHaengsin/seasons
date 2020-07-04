60fc00309d0299c32b304ac7149b82205f962164
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-07-04 07:48:16 -0600

    trying to display icons.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+    console.log(month);
+    if (month > 2 && month < 9){
+       return lat > 0 ? 'summer' : 'winter';
+    }
+    else {
+        return lat > 0 ? 'winter' : 'summer'
+    }
+
+}
 
 const SeasonDisplay = (props) => {
-    return <div>Season Display {props.lat}</div>;
+
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'winter' ? 'Bur, it\'s chilly' : 'It\'s flippin hot';
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
+    
+    return (
+        <div>
+            <i className={`${icon} icon`} />
+            <h1 align="Center">{ text }</h1>
+            <i className={`${icon} icon`} />
+        </div>
+    );
 }
 
 export default SeasonDisplay;
