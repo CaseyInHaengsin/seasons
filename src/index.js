130765ce9630efaca5d5a6b52f47faa3a5cbd2ad
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-07-04 07:26:41 -0600

    this commit demonstrates not using a constructor.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
-
 class App extends React.Component {
-    constructor(props){
-        super(props);
+    // constructor(props){
+    //     super(props);
 
-        this.state = {
-            lat: null,
-            errorMessage: ""
-        };
+    //     this.state = {
+    //         lat: null,
+    //         errorMessage: ""
+    //     };
+        
+    // }
+    state = {
+        lat: null,
+        errorMessage: ""
+    }
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({lat: position.coords.latitude})
             },
             err => this.setState({errorMessage: err.message})
         )
-    }
-
-    componentDidMount(){
         console.log('My component was rendered to the screen.')
     }
 
