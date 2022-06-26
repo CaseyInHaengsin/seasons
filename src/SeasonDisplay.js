8c85233adbc95ef98c4f15fc08155e368a124a72
caseyinhaengsin <rowley.james74@gmail.com>
2022-06-26 15:18:26 -0600

    changing things for deploy

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,42 +1,37 @@
-import './SeasonDisplay.css';
-import React from 'react';
-
+import React from 'react'
+import './SeasonDisplay.css'
 
 const seasonConfig = {
-    summer: {
-        text: "Let's hit the beach",
-        iconName: "sun"
-    },
-    winter: {
-        text: "Burr It is cold!",
-        iconName: "snowflake"
-    }
+  summer: {
+    text: "Let's hit the beach",
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr It is cold!',
+    iconName: 'snowflake'
+  }
 }
-
 
 const getSeason = (lat, month) => {
-    console.log(month);
-    if (month > 2 && month < 9){
-       return lat > 0 ? 'summer' : 'winter';
-    }
-    else {
-        return lat > 0 ? 'winter' : 'summer'
-    }
-
+  console.log(month)
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter'
+  } else {
+    return lat > 0 ? 'winter' : 'summer'
+  }
 }
 
-const SeasonDisplay = (props) => {
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth())
+  const { text, iconName } = seasonConfig[season]
 
-    const season = getSeason(props.lat, new Date().getMonth());
-    const {text, iconName } = seasonConfig[season];
-    
-    return (
-        <div className={`season-display ${season}`}>
-            <i className={`icon-left massive ${iconName} icon`} />
-            <h1 align="Center">{ text }</h1>
-            <i className={`icon-right massive ${iconName} icon`} />
-        </div>
-    );
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1 align='Center'>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  )
 }
 
-export default SeasonDisplay;
+export default SeasonDisplay
